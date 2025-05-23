95b48a5003193891e648079db388d98fa8b6da77
Heily <heilyrodriguez225@gmail.com>
2025-05-22 22:25:35 -0400

    Clase program para probar el lexer

--- Interpreter.cs
+++ Interpreter.cs
@@ -1,13 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System;
+
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        string code = "var x = 42 + (10.5 / 2);";
-        Lexer lexer = new Lexer(code);
-       
-    
+        string testCode =
+        @"Spawn ← 42
+         Color ← ""verde#00FF00""
+         width ← 1280.5
+         DrawCircle (30 * 2) + width, 720 / 2
+         GoTo 150, 300
+         IsCanvasColor ← 3.14 == (50 % 4) && True";
+
+        Lexer lexer = new Lexer(testCode);
+        //Console.WriteLine(testCode.Length);
+        Console.WriteLine("Tokens generados:");
+        
+        foreach (Token token in lexer.tokens)
+        {
+            Console.WriteLine(token);
+        }
     }
 }

e0dd8257fda67d68979c3b53e2a6704c6844d8bd
Heily <heilyrodriguez225@gmail.com>
2025-06-15 17:42:18 -0400

    implementacion de CanvasResizeInput

--- UI/CanvasResize.cs
+++ UI/CanvasResize.cs
@@ -1,7 +1,48 @@
 using Godot;
-using System;
 
-public class CanvasResize : Button
+public class CanvasResizeInput : LineEdit
 {
+    // Señal para notificar cambios de tamaño
+    [Signal] public delegate void SizeChanged(int newSize);
+    private int _minSize = 1;
+
+    public override void _Ready()
+    {
+        Text = "100";
+        
+        Connect("text_entered", this, nameof(OnTextEntered));
+        Connect("focus_exited", this, nameof(OnFocusExited));
+    }
+    private void OnTextEntered(string newText)
+    {
+        ApplyNewSize(newText);
+        ReleaseFocus();  // Quitar foco después de aplicar
+    }
+    private void OnFocusExited()
+    {
+        ApplyNewSize(Text);
+    }
+    private void ApplyNewSize(string sizeText)
+    {
+        if (!int.TryParse(sizeText, out int newSize))
+        {
+            RestoreValidSize();
+            return;
+        }
+        if (newSize < _minSize)
+        {
+            newSize = _minSize;
+        }
+        EmitSignal(nameof(SizeChanged), newSize);
     
+        Text = newSize.ToString();
+    }
+
+    private void RestoreValidSize()
+    {
+        if (!int.TryParse(Text, out int currentSize))
+        {
+            Text = "100";  // Valor por defecto
+        }
+    }
 }
